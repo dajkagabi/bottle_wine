0b9d0fdf19fa6debddd1ef47d0ea17fc29f228fb
dajkagabi <dajkagabi01@gmail.com>
2025-08-15 15:20:04 +0200

    filter

--- src/components/WineFilter/WineFilter.jsx
+++ src/components/WineFilter/WineFilter.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import WineCard from '../WineCard/WineCard';
-import WineData from '../data/WineData';
+import WineData from '../data/winedata';
 
 
 
